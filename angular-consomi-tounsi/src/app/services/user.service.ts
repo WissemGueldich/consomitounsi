42a9ee0d28a5607f974c84390451b89f77100b35
wissem1995 <gueldich.wissem@esprit.tn>
2021-05-08 08:59:34 +0100

    css

--- angular-consomi-tounsi/src/app/services/user.service.ts
+++ angular-consomi-tounsi/src/app/services/user.service.ts
@@ -22,4 +22,20 @@ export class UserService {
   getAdminBoard(): Observable<any> {
     return this.http.get(API_URL + 'admin', { responseType: 'text' });
   }
+
+  getUsers():Observable<any> {
+    return this.http.get('http://localhost:8081/user/getusers', { responseType: 'text' });
+  }
+
+  getUser(id):Observable<any> {
+    return this.http.get(API_URL + 'http://localhost:8081/user/getuser/'+id, { responseType: 'text' });
+  }
+  
+  updateUser(user):Observable<any> {
+    return this.http.put(API_URL + 'http://localhost:8081/user/updateuser/', { responseType: 'text' });
+  }
+
+  deleteUser(id):Observable<any> {
+    return this.http.delete(API_URL + 'http://localhost:8081/user/getuser/'+id, { responseType: 'text' });
+  }
 }
---
+++ src/main/java/tn/esprit/consomitounsi/service/UserService.java
@@ -0,0 +1,46 @@
+package tn.esprit.consomitounsi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.consomitounsi.modal.User;
+import tn.esprit.consomitounsi.repository.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public ResponseEntity<?> addUser(User user) {
+		if (userRepository.existsByEmail(user.getEmail()) != null) {
+			return new ResponseEntity<>("Email already taken !", HttpStatus.FOUND);
+		} else {
+			if (userRepository.findByUsername(user.getUsername()) != null) {
+				return new ResponseEntity<>("UserName already taken !", HttpStatus.FOUND);
+			}
+		}
+		return new ResponseEntity<User>(userRepository.save(user), HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<?> getUsers() {
+		return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+	}
+
+	public ResponseEntity<?> getUser(Long id) {
+		return new ResponseEntity<>(userRepository.findById(id).get(), HttpStatus.OK);
+	}
+
+	public ResponseEntity<?> updateUser(User user) {
+		return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+	}
+
+	public ResponseEntity<?> deleteUser(Long id) {
+		userRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+
+	}
+
+
+}
